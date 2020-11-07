f59d583cc56aa42d1d4574a5ce9830931487e951
Guillermo <GuillermoX7@gmail.com>
2020-11-07 15:20:06 +0100

    First commit

---
+++ src/main/java/io/github/fourfantastics/standby/StandbyApplication.java
@@ -0,0 +1,13 @@
+package io.github.fourfantastics.standby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandbyApplication.class, args);
+	}
+
+}
