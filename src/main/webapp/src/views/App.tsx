0adbcc388dbd938ec018d54049e2bc2f427dd3bd
George Laurentiu <bogdan.lorenzo11@gmail.com>
2020-11-07 19:24:29 +0200

    feat: added router

--- src/main/webapp/src/views/App.tsx
+++ src/main/webapp/src/views/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Loading } from "../components/pure/Loading";
+import { AppRouter } from "../components/Router/AppRouter";
+import { routes } from "../components/Router/routes";
 
 export const App = React.memo(() => {
-    return <div>Initial commit</div>
+  return (
+    <React.Suspense fallback={<Loading />}>
+      <AppRouter routes={routes} />
+    </React.Suspense>
+  );
 });
---
+++ src/main/webapp/src/views/Films.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import {RouteComponentProps} from "@reach/router";
+
+export const Films = React.memo<RouteComponentProps>((props) => {
+
+    return <h1>Film view</h1>
+})
