9d92f441994c969aae46659dfa529f9b7504513c
CarmenMariaMP <62175263+CarmenMariaMP@users.noreply.github.com>
2020-11-18 21:45:18 +0100

    Role service updated

--- src/main/java/io/github/fourfantastics/standby/repository/CommentRepository.java
+++ src/main/java/io/github/fourfantastics/standby/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package io.github.fourfantastics.standby.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+
+
 import io.github.fourfantastics.standby.model.Comment;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
