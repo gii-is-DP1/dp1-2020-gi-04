52c3f07a8b0119a3f726094c397445f10ed3f7b7
Fco-bit <56715417+Fco-bit@users.noreply.github.com>
2020-11-18 21:41:21 +0100

    Base Entity

--- src/main/java/io/github/fourfantastics/standby/model/BaseEntity.java
+++ src/main/java/io/github/fourfantastics/standby/model/BaseEntity.java
@@ -4,11 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Entity
 @Data
@@ -17,8 +15,8 @@ public class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long id;
-	
+
 	public boolean isNew() {
-		return this.id== null;
+		return this.id == null;
 	}
 }
