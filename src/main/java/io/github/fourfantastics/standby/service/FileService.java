0ad2fa18b24576dc282680fd694151fabf33560e
Guillermo <GuillermoX7@gmail.com>
2020-12-16 19:26:54 +0100

    Added format constraint to file upload

--- src/main/java/io/github/fourfantastics/standby/service/FileService.java
+++ src/main/java/io/github/fourfantastics/standby/service/FileService.java
@@ -2,6 +2,8 @@ package io.github.fourfantastics.standby.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import io.github.fourfantastics.standby.service.exceptions.InvalidExtensionException;
+
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
@@ -10,7 +12,7 @@ import org.springframework.core.io.Resource;
 public interface FileService {
 	public void init();
 
-	public String save(MultipartFile file);
+	public String save(MultipartFile file) throws InvalidExtensionException;
 
 	public Resource load(String filename);
 
