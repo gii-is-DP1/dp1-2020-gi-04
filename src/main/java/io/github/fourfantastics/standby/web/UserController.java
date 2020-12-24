bd2b86f049cc7c368daa97388a55574bb29445d9
Fco-bit <pacozf14@gmail.com>
2020-12-24 16:26:44 +0100

    Merge branch 'frarodper4' of github.com:gii-is-DP1/dp1-2020-gi-04 into frarodper4
    # Conflicts:
    #	src/test/java/io/github/fourfantastic/standby/service/UserServiceTest.java

--- src/main/java/io/github/fourfantastics/standby/web/UserController.java
+++ src/main/java/io/github/fourfantastics/standby/web/UserController.java
@@ -23,7 +23,6 @@ import io.github.fourfantastics.standby.model.form.FilmmakerConfigurationData;
 import io.github.fourfantastics.standby.model.validator.CompanyConfigurationDataValidator;
 import io.github.fourfantastics.standby.model.validator.CredentialsValidator;
 import io.github.fourfantastics.standby.model.validator.FilmmakerConfigurationDataValidator;
-import io.github.fourfantastics.standby.service.NotificationConfigurationService;
 import io.github.fourfantastics.standby.service.UserService;
 import io.github.fourfantastics.standby.service.exception.DataMismatchException;
 import io.github.fourfantastics.standby.service.exception.NotFoundException;
@@ -32,9 +31,6 @@ import io.github.fourfantastics.standby.service.exception.NotFoundException;
 public class UserController {
 	@Autowired
 	UserService userService;
-
-	@Autowired
-	NotificationConfigurationService notificationConfigurationService;
 	
 	@Autowired
 	CredentialsValidator credentialsValidator;
