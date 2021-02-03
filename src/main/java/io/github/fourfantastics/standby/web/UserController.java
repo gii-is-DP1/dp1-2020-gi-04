2bc50afd0872ede238bbdc2cec0158cd3c12ac17
Guillermo <GuillermoX7@gmail.com>
2021-02-03 19:37:58 +0100

    Fixed logout page

--- src/main/java/io/github/fourfantastics/standby/web/UserController.java
+++ src/main/java/io/github/fourfantastics/standby/web/UserController.java
@@ -2,9 +2,13 @@ package io.github.fourfantastics.standby.web;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,6 +48,15 @@ public class UserController {
 		model.put("credentials", new Credentials());
 		System.out.println(bindingResult.getAllErrors());
 		return "login";
+	}
+	
+	@GetMapping("/logout")
+	public String doLogout(HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login";
 	}
 
 	@GetMapping("/account")
