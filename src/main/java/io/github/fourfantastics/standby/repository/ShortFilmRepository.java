70ffc80735cd1e7580b589b47bdec6b0b90f8228
CarmenMariaMP <62175263+CarmenMariaMP@users.noreply.github.com>
2021-02-04 19:35:23 +0100

    followedShortFilms in ShortFilmRepository fixed

--- src/main/java/io/github/fourfantastics/standby/repository/ShortFilmRepository.java
+++ src/main/java/io/github/fourfantastics/standby/repository/ShortFilmRepository.java
@@ -18,13 +18,17 @@ public interface ShortFilmRepository extends CrudRepository<ShortFilm, Long> {
 
 	public Page<ShortFilm> findByUploader(Filmmaker uploader, Pageable pageable);
 
-	@Query(value = "SELECT shortfilm from ShortFilm shortfilm JOIN shortfilm.roles roles JOIN roles.filmmaker filmmaker WHERE filmmaker.id = :filmmakerId", countQuery = "SELECT count(shortfilm) from ShortFilm shortfilm JOIN shortfilm.roles roles JOIN roles.filmmaker filmmaker WHERE filmmaker.id = :filmmakerId", nativeQuery = false)
-	public Page<ShortFilm> findAttachedShortFilmByFilmmaker(@Param("filmmakerId") Long filmmakerId, Pageable pageable);
-
 	@Query("SELECT count(shortfilm) from ShortFilm shortfilm JOIN shortfilm.roles roles JOIN roles.filmmaker filmmaker WHERE filmmaker.id = :filmmakerId")
 	public Integer countAttachedShortFilmByFilmmaker(@Param("filmmakerId") Long filmmakerId);
+	
+	@Query(value = "SELECT shortfilm from ShortFilm shortfilm JOIN shortfilm.roles roles JOIN roles.filmmaker filmmaker WHERE filmmaker.id = :filmmakerId", 
+			countQuery = "SELECT count(shortfilm) from ShortFilm shortfilm JOIN shortfilm.roles roles JOIN roles.filmmaker filmmaker WHERE filmmaker.id = :filmmakerId", 
+			nativeQuery = false)
+	public Page<ShortFilm> findAttachedShortFilmByFilmmaker(@Param("filmmakerId") Long filmmakerId, Pageable pageable);
 
-	@Query(value = "SELECT shortfilm from User user JOIN user.filmmakersSubscribedTo subscribed JOIN subcribed.uploadedShortFilms shortfilm WHERE user.id = :userId", countQuery = "SELECT count(shortfilm) from User user JOIN user.filmmakersSubscribedTo subscribed JOIN subcribed.uploadedShortFilms shortfilm WHERE user.id = :userId", nativeQuery = false)
+	@Query(value = "SELECT shortfilm from User user JOIN user.filmmakersSubscribedTo subscribed JOIN subscribed.uploadedShortFilms shortfilm WHERE user.id = :userId", 
+			countQuery = "SELECT count(shortfilm) from User user JOIN user.filmmakersSubscribedTo subscribed JOIN subscribed.uploadedShortFilms shortfilm WHERE user.id = :userId", 
+			nativeQuery = false)
 	public Page<ShortFilm> followedShortFilms(@Param("userId") Long userId, Pageable pageable);
 
 }
