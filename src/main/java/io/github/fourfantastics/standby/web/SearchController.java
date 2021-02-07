dfab8b753eb23c83efb6ae738e1b0a8a9939b427
George Laurentiu <bogdan.lorenzo11@gmail.com>
2021-02-08 00:27:14 +0100

    added search filmmaker

--- src/main/java/io/github/fourfantastics/standby/web/SearchController.java
+++ src/main/java/io/github/fourfantastics/standby/web/SearchController.java
@@ -10,8 +10,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.github.fourfantastics.standby.model.Filmmaker;
 import io.github.fourfantastics.standby.model.ShortFilm;
 import io.github.fourfantastics.standby.model.form.SearchData;
+import io.github.fourfantastics.standby.service.FilmmakerService;
 import io.github.fourfantastics.standby.service.ShortFilmService;
 import io.github.fourfantastics.standby.service.exception.BadRequestException;
 
@@ -20,19 +22,25 @@ public class SearchController {
 	
 	@Autowired
 	ShortFilmService shortFilmService;
+	
+	@Autowired
+	FilmmakerService filmmakerService;
 
 	@RequestMapping("/search")
 	public String search(@ModelAttribute SearchData searchData, BindingResult result, Map<String, Object> model) {
 
 		List<ShortFilm> shortFilms = new ArrayList<ShortFilm>();
+		List<Filmmaker> filmmakers = new ArrayList<Filmmaker>();
 		try {
 			shortFilms = shortFilmService.searchShortFilms(searchData).getContent();
+			filmmakers = filmmakerService.findFirst3ByNameLike(searchData.getQ());
 		} catch (BadRequestException e) {
 			return "redirect:/";
 		}
 		
 		model.put("search", searchData);
 		model.put("shortFilms", shortFilms);
+		model.put("filmmakers", filmmakers);
 		return "search";
 	}
 }
