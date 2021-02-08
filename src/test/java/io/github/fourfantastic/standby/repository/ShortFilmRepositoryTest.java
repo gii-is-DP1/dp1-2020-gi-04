df9898e05bb02befec1916b6e159712bf3a9dd46
Fco-bit <pacozf14@gmail.com>
2021-02-08 13:53:07 +0100

    ShortFim Repository Attached ShortFilm querys test

--- src/test/java/io/github/fourfantastic/standby/repository/ShortFilmRepositoryTest.java
+++ src/test/java/io/github/fourfantastic/standby/repository/ShortFilmRepositoryTest.java
@@ -3,22 +3,26 @@ package io.github.fourfantastic.standby.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.List;
 import io.github.fourfantastics.standby.StandbyApplication;
 import io.github.fourfantastics.standby.filters.ShortFilmSpecifications;
 import io.github.fourfantastics.standby.model.Filmmaker;
+import io.github.fourfantastics.standby.model.Role;
+import io.github.fourfantastics.standby.model.RoleType;
 import io.github.fourfantastics.standby.model.ShortFilm;
 import io.github.fourfantastics.standby.model.Tag;
 import io.github.fourfantastics.standby.model.form.DateFilter;
@@ -26,6 +30,7 @@ import io.github.fourfantastics.standby.model.form.Pagination;
 import io.github.fourfantastics.standby.model.form.SearchData;
 import io.github.fourfantastics.standby.model.form.SortType;
 import io.github.fourfantastics.standby.repository.FilmmakerRepository;
+import io.github.fourfantastics.standby.repository.RoleRepository;
 import io.github.fourfantastics.standby.repository.ShortFilmRepository;
 import io.github.fourfantastics.standby.repository.TagRepository;
 import io.github.fourfantastics.standby.service.RatingService;
@@ -51,7 +56,78 @@ public class ShortFilmRepositoryTest {
 	
 	@Autowired
 	ShortFilmService shortFilmService;
-
+	
+	@Autowired
+	RoleRepository rolesRepository;
+	
+	@Test 
+	void countAttachedShortFilmByFilmmaker(){
+		Filmmaker filmmaker = new Filmmaker();
+		filmmaker.setName("filmmaker1");
+		filmmaker.setPassword("password");
+		filmmaker.setEmail("filmmaker@gmail.com");
+		filmmaker.setPhotoUrl(null);
+		filmmaker.setCity("Seville");
+		filmmaker.setCountry("Spain");
+		filmmaker.setFullname("Filmmaker Díaz García");
+		filmmaker.setPhone("675987432");
+		filmmaker.setCreationDate(new Date().getTime());
+		filmmakerRepository.save(filmmaker);
+		
+		ShortFilm shortFilm = new ShortFilm();
+		shortFilm.setTitle("Test film");
+		shortFilm.setVideoUrl("example.mp4");
+		shortFilm.setUploadDate(8L);
+		shortFilm.setDescription("");
+		shortFilm.setUploader(filmmaker);
+		shortFilm.setViewCount(3L);
+		shortFilmRepository.save(shortFilm);
+		
+		Role role = new Role();
+		role.setFilmmaker(filmmaker);
+		role.setRole(RoleType.ACTOR);
+		role.setShortfilm(shortFilm);
+		rolesRepository.save(role);
+		
+		assertEquals(shortFilmRepository.countAttachedShortFilmByFilmmaker(filmmaker.getId()), 1);
+	}
+	
+	@Test 
+	void getAttachedShortFilmByFilmmaker(){
+		Filmmaker filmmaker = new Filmmaker();
+		filmmaker.setName("filmmaker1");
+		filmmaker.setPassword("password");
+		filmmaker.setEmail("filmmaker@gmail.com");
+		filmmaker.setPhotoUrl(null);
+		filmmaker.setCity("Seville");
+		filmmaker.setCountry("Spain");
+		filmmaker.setFullname("Filmmaker Díaz García");
+		filmmaker.setPhone("675987432");
+		filmmaker.setCreationDate(new Date().getTime());
+		filmmakerRepository.save(filmmaker);
+		
+		ShortFilm shortFilm = new ShortFilm();
+		shortFilm.setTitle("Test film");
+		shortFilm.setVideoUrl("example.mp4");
+		shortFilm.setUploadDate(8L);
+		shortFilm.setDescription("");
+		shortFilm.setUploader(filmmaker);
+		shortFilm.setViewCount(3L);
+		shortFilmRepository.save(shortFilm);
+		
+		Role role = new Role();
+		role.setFilmmaker(filmmaker);
+		role.setRole(RoleType.ACTOR);
+		role.setShortfilm(shortFilm);
+		rolesRepository.save(role);
+		
+		List<ShortFilm> resultExpected = new ArrayList<ShortFilm>();
+		resultExpected.add(shortFilmRepository.findById(shortFilm.getId()).get());
+		
+		assertEquals(shortFilmRepository.findAttachedShortFilmByFilmmaker(filmmaker.getId(), Pagination.empty().getPageRequest()).getContent(),
+				resultExpected);
+	}
+	
 	@Test
 	void getShortFilmsByName() {
 
